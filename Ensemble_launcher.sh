dbc7956588721c7e7be2911af08a4e69f787b693
Ezequiel de la Rosa <edelarosa@ip-10-0-3-248.eu-west-1.compute.internal>
2023-12-22 09:23:48 +0000

    Add algorithm docker references and contact details

--- Ensemble_launcher.sh
+++ Ensemble_launcher.sh
@@ -1,12 +1,15 @@
-# run seals
+# Run SEALS Docker (https://github.com/Tabrisrei/ISLES22_SEALS)
+# Contact person: Shengbo Gao (GTabris@buaa.edu.cn)
 cd SEALS
 bash nnunet_launcher.sh
 
-# run nvauto
+# Run NVAUTO Docker (https://github.com/mahfuzmohammad/isles22)
+ # Contact person: Md Mahfuzur Rahman Siddiquee (mrahmans@asu.edu)
 cd ../NVAUTO
 python process.py
 
-# run factorizer
+# Run SWAN Docker (https://github.com/pashtari/factorizer-isles22)
+# Contact person: Pooya Ashtari (pooya.ashtari@esat.kuleuven.be)
 cd ../FACTORIZER
 python process.py
 
--- isles22_ensemble.py
+++ isles22_ensemble.py
@@ -2,15 +2,36 @@ import os
 import shutil
 import subprocess
 
-def print_run(algorithm):
+# Author: Ezequiel de la Rosa (ezequieldlrosa@gmail.com)
+# 22.12.2023
 
+def print_run(algorithm):
     print('#########################################################')
     print('#########################################################')
     print('Running {} algorithm ...'.format(algorithm))
     print('#########################################################')
     print('#########################################################')
 
+
 def predict_ensemble(isles_ensemble_path, input_dwi_path, input_adc_path, input_flair_path, output_path, save_team_outputs=False):
+    ''' Runs the Isles'22 Ensemble algorithm.
+    
+    Inputs:
+    
+    isles_ensemble_path: path to isles'22 ensemble git repo
+    
+    input_dwi_path: path to DWI image in nifti format
+    
+    input_adc_path: path to ADC image in nifti format
+    
+    input_flair_path: path to FLAIR image in nifti format
+    
+    output_path: path where stroke lesion mask output is stored
+    
+    save_team_outputs: True for storing the non-ensembled, individual algorithm results
+    
+    ''' 
+    
     # Create folders to store inputs & intermediate results.
     if os.path.exists(os.path.join(isles_ensemble_path, 'input')):
         shutil.rmtree(os.path.join(isles_ensemble_path, 'input'))
@@ -39,27 +60,34 @@ def predict_ensemble(isles_ensemble_path, input_dwi_path, input_adc_path, input_
 
 def predict(isles_ensemble_path, input_dwi_path):
 
-    # Run seals.
+    # Run SEALS Docker (https://github.com/Tabrisrei/ISLES22_SEALS)
+    # Contact person: Shengbo Gao (GTabris@buaa.edu.cn)
+     
     print_run('SEALS')
     path_seals = isles_ensemble_path + 'SEALS/'
     command_seals = path_seals
     command_seals += f'nnunet_launcher.sh'
     subprocess.run(command_seals, shell=True, cwd=path_seals)
 
-    # run nvauto
+    # Run NVAUTO Docker (https://github.com/mahfuzmohammad/isles22)
+    # Contact person: Md Mahfuzur Rahman Siddiquee (mrahmans@asu.edu)
+    
     print_run('NVAUTO')
     path_nvauto = isles_ensemble_path + 'NVAUTO/'
     command_nvauto = f'python process.py'
     subprocess.run(command_nvauto, shell=True, cwd=path_nvauto)
 
-    # # run swan (factorizer)
+    # Run SWAN Docker (https://github.com/pashtari/factorizer-isles22)
+    # Contact person: Pooya Ashtari (pooya.ashtari@esat.kuleuven.be)
+
     print_run('SWAN')
     path_factorizer = isles_ensemble_path + 'FACTORIZER/'
     command_factorizer = f'python process.py'
     subprocess.run(command_factorizer, shell=True, cwd=path_factorizer)
 
+    # Ensembling results.
+
     print_run('majority voting')
-    # # # run majority voting
     path_voting = isles_ensemble_path
     command_voting = f'python majority_voting.py -i output_teams/ -o output/images/stroke-lesion-segmentation/'
     subprocess.call(command_voting, shell=True, cwd=path_voting)
