ea9281c5ded95d4f9344e4841b7cc8db9e0fb710
Ezequiel de la Rosa <edelarosa@ip-10-0-3-100.eu-west-1.compute.internal>
2024-04-03 12:15:19 +0000

    Add example case.

--- scripts/predict_scan.py
+++ scripts/predict_scan.py
@@ -1,24 +1,20 @@
-# author: Ezequiel de la Rosa (ezequieldlrosa@gmail.com)
-# 22.12.2023
+# Author: Ezequiel de la Rosa (ezequieldlrosa@gmail.com)
+# 03.04.2023
 
+import os
 import sys
-ENSEMBLE_PATH = 'path-to-isles-ensemble-repo/' 
-sys.path.append(ENSEMBLE_PATH)
 from isles22_ensemble import predict_ensemble
+ENSEMBLE_PATH = os.path.dirname(os.getcwd())                                                   # path-to-ensemble-repo
+sys.path.append(ENSEMBLE_PATH)
 
+INPUT_FLAIR = os.path.join(ENSEMBLE_PATH, 'data', 'sub-strokecase0001_ses-0001_flair.nii.gz')  # path-to-FLAIR
+INPUT_DWI = os.path.join(ENSEMBLE_PATH, 'data', 'sub-strokecase0001_ses-0001_dwi.nii.gz')      # pat-t-DWI
+INPUT_ADC = os.path.join(ENSEMBLE_PATH, 'data', 'sub-strokecase0001_ses-0001_adc.nii.gz')      # path-to-ADC
+OUTPUT_PATH = os.path.join(ENSEMBLE_PATH, 'example_test')                                      # path-to-output
 
-if __name__ == "__main__":
-
-    ''' Example script to run The Isles'22 Ensemble algorithm over a scan.'''
-
-    INPUT_FLAIR = 'path-to-flair.nii.gz'
-    INPUT_ADC = 'path-to-adc.nii.gz'
-    INPUT_DWI = 'path-to-dwi.nii.gz'
-    OUTPUT_PATH = 'path-to-output-folder'
-
-    predict_ensemble(isles_ensemble_path=ENSEMBLE_PATH,
-                         input_dwi_path=INPUT_DWI,
-                        input_adc_path=INPUT_ADC,
-                        input_flair_path=INPUT_FLAIR,
-                        output_path=OUTPUT_PATH,
-                        save_team_outputs=False)
+predict_ensemble(isles_ensemble_path=ENSEMBLE_PATH,
+                 input_dwi_path=INPUT_DWI,
+                 input_adc_path=INPUT_ADC,
+                 input_flair_path=INPUT_FLAIR,
+                 output_path=OUTPUT_PATH,
+                 save_team_outputs=False)
